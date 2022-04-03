4fdd145c80e3cbb1b5641c682e906afb31fb17ab
shwetaps605 <shwetaroy305@gmail.com>
2022-04-03 20:31:46 +0530

    understood the counter example

--- src/App.js
+++ src/App.js
@@ -1,36 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import { useSelector, useDispatch } from 'react-redux'
-// import { increment, decrement, signIn } from './actions'
-// import { addBug, removeBug } from './actions'
 import Counter from './components/Counter';
 
 
 
 function App() {
-  
-  
-
   return (
     <div className="App">
       <Counter/>
-      {/* <header>
-        <h2>Counter:{counter}</h2>
-        <button onClick={()=> dispatch(signIn())}>Log In</button>
-      </header>
-
-      <main>
-        <button onClick= {()=> dispatch(increment(5))}>+</button>
-        <button onClick= {()=> dispatch(decrement())}>-</button>
-      </main>
-
-      {
-        isLogged && 
-        <section>
-            <p>Valuable Information</p>
-        </section>
-      } */}
-
     </div>
   );
 }
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,20 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    count: 0
+}
+
 export const counterSlice = createSlice({
     name:"counter",
-    initialState:{
-        count: 0
-    },
+    initialState,
     reducers:{
         incremented: state =>{ state.count += 1},
         decremented: state => { state.count -= 1    },
-        incrementBy: (state,action) =>{ state.value += action.payload.value }
+        incrementBy: (state,action) =>{ state.count += action.payload },
+        reset: () => initialState,
+        // reset: () =>({...initialState}),
+        // reset: state => Object.assign(state,initialState)
     }
 })
 
 
-
-export const { incremented, decremented, incrementBy} = counterSlice.actions
+export const { incremented, decremented, incrementBy, reset} = counterSlice.actions
 
 //The below function is called a selector and allows us to select a value from the state.
 //Selectors can also be defined inline where they're used instead of in the slice file
